15e44b2e17c05c8cbc4b7c53b489048203c7c3ad
nabi-ai <ajgurnabiullina@yandex.ru>
2024-10-06 18:33:34 +0300

    mesto.first

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template').content;
+
+
 // @todo: DOM узлы
+
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
+const createCard = (name, link, onRemoveCallback) => {
+    const cardElement = cardTemplate.querySelector('.places__item.card').cloneNode(true);
+
+    cardElement.querySelector('.card__image').src = link;
+    cardElement.querySelector('.card__title').textContent = name;
+    cardElement.querySelector('.card__delete-button').addEventListener('click', () => onRemoveCallback(cardElement));
+
+    return cardElement;
+}
+
 // @todo: Функция удаления карточки
 
+const removeCard = (domElement) => domElement.remove();
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach((item) => {
+    placesList.append(createCard(item.name, item.link, removeCard));
+});
